ab0d3cb69607962308da952476a23886b5726ac5
Dennie Mok <mokdennie26@gmail.com>
2023-08-29 21:47:36 +1000

    Added util

---
+++ src/com/battleasya/Hdlr/Util.java
@@ -0,0 +1,32 @@
+package com.battleasya.Hdlr;
+
+import com.battleasya.StaffChat;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import java.util.Map;
+
+public class Util {
+
+    private final StaffChat plugin;
+
+    public Util(StaffChat plugin) {
+        this.plugin = plugin;
+    }
+
+    public static void msgSender(CommandSender sender, String message) {
+        sender.sendMessage(ChatColor.translateAlternateColorCodes('&',message));
+    }
+
+    public void msgStaff(String msg) {
+        for (Map.Entry<String, Integer> entry : plugin.staffList.entrySet()) {
+            Player player = Bukkit.getPlayer(entry.getKey());
+            if (player != null) {
+                Util.msgSender(player, msg);
+            }
+        }
+    }
+
+}

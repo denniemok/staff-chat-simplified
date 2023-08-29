4ba73ad525b7d325343428398631b38fdf956f84
Dennie Mok <mokdennie26@gmail.com>
2023-08-29 21:48:35 +1000

    Merged event handlers

---
+++ src/com/battleasya/Hdlr/Event.java
@@ -0,0 +1,53 @@
+package com.battleasya.Hdlr;
+
+import com.battleasya.StaffChat;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.*;
+
+public class Event implements Listener {
+
+    private final StaffChat plugin;
+
+    public Event(StaffChat plugin) {
+        this.plugin = plugin;
+    }
+
+    @EventHandler
+    public void onChat(AsyncPlayerChatEvent event) {
+
+        String playerName = event.getPlayer().getName();
+
+        if (plugin.chatToggleList.containsKey(playerName)) {
+            String message = event.getMessage();
+            event.setCancelled(true);
+            plugin.util.msgStaff(plugin.config.chatMessage
+                    .replaceAll("%name%", playerName)
+                    .replaceAll("%message%",  message));
+        }
+
+    }
+
+    @EventHandler
+    public void onJoin(PlayerJoinEvent event){
+
+        Player player = event.getPlayer();
+
+        if (player.hasPermission("staffchat.use")) {
+            plugin.staffList.put(player.getName(), 1);
+        }
+
+    }
+
+    @EventHandler
+    public void onLeave(PlayerQuitEvent event){
+
+        String playerName = event.getPlayer().getName();
+
+        plugin.staffList.remove(playerName);
+        plugin.chatToggleList.remove(playerName);
+
+    }
+
+}

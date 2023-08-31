a4f74797e4637c9dcf145d32e4d5346d61facb0b
Dennie Mok <mokdennie26@gmail.com>
2023-08-31 19:23:25 +1000

    Removed unused import statement

--- src/com/battleasya/staffchat/handler/Event.java
+++ src/com/battleasya/staffchat/handler/Event.java
@@ -1,6 +1,5 @@
 package com.battleasya.staffchat.handler;
 
-import com.battleasya.staffchat.StaffChat;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;

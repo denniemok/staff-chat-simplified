c2d12ac7a9056035017a28098961021751566ba0
Dennie Mok <mokdennie26@gmail.com>
2023-08-31 19:09:59 +1000

    Regrouped variables and added new options

--- src/com/battleasya/staffchat/handler/Config.java
+++ src/com/battleasya/staffchat/handler/Config.java
@@ -11,23 +11,38 @@ public class Config {
         this.plugin = plugin;
     }
 
-    public String noPermission;
-    public String commandSyntax;
-    public String chatMessage;
-    public String toggleEnable;
-    public String toggleDisable;
-    public String reloadConfig;
+    public static String chatMessage;
+
+    public static String toggleEnable;
+
+    public static String toggleDisable;
+
+    public static String noPermission;
+
+    public static String messageSyntax;
+
+    public static String toggleSyntax;
+
+    public static String reloadSyntax;
+
+    public static String reloadConfig;
 
     /* load config into memory */
     public void fetchConfig() {
 
         FileConfiguration config = plugin.getConfig();
 
-        noPermission = config.getString("no-permission");
-        commandSyntax = config.getString("command-syntax");
         chatMessage = config.getString("chat-message");
+
         toggleEnable = config.getString("toggle-enable");
         toggleDisable = config.getString("toggle-disable");
+
+        noPermission = config.getString("no-permission");
+
+        messageSyntax = config.getString("message-syntax");
+        toggleSyntax = config.getString("toggle-syntax");
+        reloadSyntax = config.getString("reload-syntax");
+
         reloadConfig = config.getString("reload-config");
 
     }
